1b792f6daa0bebb63eb4f1788b26a6630c2c829d
Admin <nik.gora.2004@gmail.com>
2022-06-16 20:25:57 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/CStringT2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CStringT2.dir/main.c.obj"
+  "CMakeFiles/CStringT2.dir/main.c.obj.d"
+  "CStringT2.exe"
+  "CStringT2.exe.manifest"
+  "CStringT2.pdb"
+  "libCStringT2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CStringT2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
