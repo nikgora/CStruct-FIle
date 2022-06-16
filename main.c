9a2c09ca588ff2143f394031674afb5dd05215ec
Admin <nik.gora.2004@gmail.com>
2022-06-16 22:31:36 +0200

    field ready enum and union not ready

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#define N 100
+struct people  {
+    struct field {
+        int age;
+        char name[N];
+    } field;
+    enum sex {
+        Man,Women
+    }sex;
+}people;
 
 int main() {
+    puts("Enter your name");
+    gets(people.field.name);
+    puts("Enter your age");
+    scanf("%d",&people.field.age);
+    printf("Your name is %s.\nYou is %d years old.",people.field.name,people.field.age);
     return 0;
 }
